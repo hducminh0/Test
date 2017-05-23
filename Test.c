00148676b1f77f7374ad22e146c85e9953c862fe
hducminh0 <hduc.minhlp@gmail.com>
2017-05-23 15:24:54 +0700

    Check multiple accounts, take 2

---
+++ Test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i=0;
+	printf("Hello");
+	while(i<3)
+	{
+		++i;
+	};
+	return 0;
+}
---
+++ Testbool.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool a = true;
+int main(){
+	//printf("%d\n", a);
+	while(a)
+	{
+		printf("a=%d\n", a);
+	}
+	return 0;
+}
